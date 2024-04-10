3a450bb0ae372963e272ba540697c29fd64b4e1b
gmarsuah <gmarsuah@gmail.com>
2024-04-10 10:14:28 +0100

    Creación nuevo proyecto

---
+++ src/Zoo/Main.java
@@ -0,0 +1,11 @@
+package Zoo;
+public class Main {
+
+	public static void main(String[] args) {
+        // Zona específica para crear las instancias de animales
+		Animal tigre = new Animal();
+        // Continúa con el resto de animales...
+		tigre.verAnimal();
+		
+    }
+}
