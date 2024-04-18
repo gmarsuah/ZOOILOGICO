0f9bb673aa1579baaa3c164e6248246d37f000a2
Jorgell01 <150284797+Jorgell01@users.noreply.github.com.>
2024-04-18 12:30:17 +0100

    Hice cambios en el codigo de reptil, serpiente y main para que todo funcionara correctamente con el nuevo reordenamientos de los archivos

--- src/Zoo/Main.java
+++ src/Zoo/Main.java
@@ -6,18 +6,20 @@ import Ave.Aguila;
 import Ave.Colibri;
 import Ave.Loro;
 
+import Reptil.Serpiente;
+
 public class Main {
 
     public static void main(String[] args) {
         List<Animal> listaDeAnimales = new ArrayList<>();
 
         // Añadir animales a la lista
+        listaDeAnimales.add(new Serpiente()); //Instancia de serpiente de la clase Reptil
         listaDeAnimales.add(new Aguila());
         listaDeAnimales.add(new Loro());
         listaDeAnimales.add(new Colibri());
         listaDeAnimales.add(new Rana()); // Añade una instancia de Rana
         listaDeAnimales.add(new Salamandra()); // Añade una instancia de Anfibio genérico
-        listaDeAnimales.add(new Serpiente());
         
         // Cada alumno puede añadir su animal así:
         // listaDeAnimales.add(new SuAnimal());
