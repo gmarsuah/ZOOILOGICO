01273a4ab2b16abfb956b3e5fc6f2b7a284038de
Rub3nCP <rubenchedas12@gmail.com>
2024-04-25 16:10:16 +0100

    Agregado Javadoc a la clase Lagarto

--- src/Reptil/Lagarto.java
+++ src/Reptil/Lagarto.java
@@ -2,14 +2,20 @@ package Reptil;
 
 import java.util.Random;
 
+/**
+ * Clase que representa un Lagarto, que es un tipo de reptil.
+ */
 public class Lagarto extends Reptil {
-    protected String colorPiel;
-    protected String tamano;
+    protected String colorPiel; // Color de la piel del lagarto
+    protected String tamano; // Tamaño del lagarto
 
     private static final String[] coloresPosibles = {"Verde", "Marrón", "Gris"};
     private static final String[] tamanosPosibles = {"Pequeño", "Mediano", "Grande"};
 
     // Constructor sin parámetros
+    /**
+     * Constructor sin parámetros que crea un lagarto con valores aleatorios para color de piel y tamaño.
+     */
     public Lagarto() {
         super();
         Random random = new Random();
@@ -18,6 +24,18 @@ public class Lagarto extends Reptil {
     }
 
     // Constructor con parámetros
+    /**
+     * Constructor con parámetros que permite especificar todas las características del lagarto.
+     *
+     * @param nombre          Nombre del lagarto.
+     * @param edad            Edad del lagarto.
+     * @param peso            Peso del lagarto.
+     * @param tipoPiel        Tipo de piel del lagarto.
+     * @param esVeneno        Indica si el lagarto es venenoso o no.
+     * @param habitat         Hábitat del lagarto.
+     * @param colorPiel       Color de la piel del lagarto.
+     * @param tamano          Tamaño del lagarto.
+     */
     public Lagarto(String nombre, int edad, float peso, String tipoPiel, boolean esVeneno, String habitat, String colorPiel, String tamano) {
         super(nombre, edad, peso, tipoPiel, esVeneno, habitat);
         this.colorPiel = colorPiel;
@@ -25,6 +43,9 @@ public class Lagarto extends Reptil {
     }
 
     // Método específico del lagarto para regenerar su cola
+    /**
+     * Método que simula al lagarto regenerando su cola.
+     */
     public void regenerarCola() {
         System.out.println(nombre + " regenera su cola.");
     }
