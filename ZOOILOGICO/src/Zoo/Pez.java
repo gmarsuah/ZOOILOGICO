270caf75bef753e9b3228bd3b99d628817de01b5
GonAfMont <gonzaloafonsomon@gmail.com>
2024-04-25 21:16:31 +0100

    Javadoc Tiburon

--- ZOOILOGICO/src/Zoo/Pez.java
+++ ZOOILOGICO/src/Zoo/Pez.java
@@ -1,35 +1,34 @@
 package Zoo;
 
-// Haendel, Cristian1, Cristian2, Gonzalo.
+// Haendel, Cristian1, Cristian2, Gonzalo Afonso.
 
 import java.util.Random;
 
-public class Pez extends Animal {
+public class Pez extends Animal{
     protected String tamaño;
     protected String colorEscamas;
     protected String tipoAgua;
 
-    private String[] tamaños = { "diminuto", "pequeño", "mediano", "grande", "enorme" };
-    private String[] colores = { "azul", "rojo", "verde" };
-    private String[] agua = { "dulce", "salada" };
-
+    private String[] tamaños = {"diminuto", "pequeño", "mediano", "grande", "enorme"};
+    private String[] colores = {"azul", "rojo", "verde"};
+    private String[] agua = {"dulce", "salada"};
+    
     Random random = new Random();
 
     public Pez() {
         super();
-
+        
         this.tamaño = tamaños[random.nextInt(tamaños.length)];
         this.colorEscamas = colores[random.nextInt(colores.length)];
         this.tipoAgua = agua[random.nextInt(agua.length)];
     }
 
-    // public Pez(String nombre, String edad, String peso, String tamaño, String
-    // colorescamas, String tipoAgua) {
-    // super(nombre, edad, peso, tamaño, colorescamas, tipoAgua);
-    // this.tamaño = tamaño;
-    // this.colorEscamas = colorescamas;
-    // this.tipoAgua = tipoAgua;
-    // }
+    public Pez(String nombre, int edad, float peso, String tamaño, String colorescamas, String tipoAgua) {
+        super(nombre, edad, peso);
+        this.tamaño = tamaño;
+        this.colorEscamas = colorescamas;
+        this.tipoAgua = tipoAgua;
+    }
 
     public void nadar() {
         System.out.println(nombre + " está nadando, placidamente.");
@@ -37,15 +36,14 @@ public class Pez extends Animal {
 
     public void respirarBajoAgua() {
         System.out.println(nombre + " está respirando bajo agua.");
-        System.out.println(nombre
-                + " está abriendo y cerrando la boca para así poder hacer que el agua pase por la branqueas para asi conseguir el oxígeno");
+        System.out.println(nombre + " está abriendo y cerrando la boca para así poder hacer que el agua pase por la branqueas para asi conseguir el oxígeno");
     }
 
-    public void ponerHuevos() {
+    public void ponerHuevos()  {
         boolean cuidadoParental = random.nextBoolean();
-        if (cuidadoParental) {
+        if (cuidadoParental){
             System.out.println("Se queda cuidando los huevos, que acaba de poner");
-        } else {
+        }else {
             System.out.println("Se va a por cigarros");
         }
     }
@@ -53,12 +51,12 @@ public class Pez extends Animal {
     @Override
     public void verAnimal() {
         Runnable[] acciones = {
-                this::comer,
-                this::dormir,
-                this::moverse,
-                this::nadar,
-                this::respirarBajoAgua,
-                this::ponerHuevos,
+            this::comer,
+            this::dormir,
+            this::moverse,
+            this::nadar,
+            this::respirarBajoAgua,
+            this::ponerHuevos
         };
 
         try {
